5172e6bfcf375df4631b43423bf279d9b9a0f1f1
Jasonspd <jasoncluu@googlemail.com>
2015-03-19 05:16:42 +0000

    clean up routing (still need to make it modular)

--- api/server.js
+++ api/server.js
@@ -1,13 +1,13 @@
-var Hapi 	= require("hapi");
+var Hapi 	= require('hapi');
 var server 	= new Hapi.Server();
-var Path = require("path");
-var githubcreds = require("../githubcreds.json");
+var Path = require('path');
+var githubcreds = require('../githubcreds.json');
 
-var Joi = require("joi");
-var Bell = require("bell");
-var AuthCookie = require("hapi-auth-cookie");
+var Joi = require('joi');
+var Bell = require('bell');
+var AuthCookie = require('hapi-auth-cookie');
 
-var jade = require("jade");
+var jade = require('jade');
 
 /* $lab:coverage:off$ */
 server.connection({
@@ -24,83 +24,20 @@ server.views({
         path: Path.join(__dirname, '../views')
     });
 
-//Routes
-server.route({
-	method: 'GET',
-	path: '/jade',
-	handler: function(request, reply) {
-		reply.view('homepage', {name: "traveller"});	
-	}
-});
-
-server.route({
-    method: 'GET',
-    path: '/{id}',
-    handler: function (request, reply) {
-        	reply('Blog Post here ' + request.params.id);
-        }
-});
-
-server.route({
-    method: 'GET',
-    path: '/edit',
-    handler: function (request, reply) {
-        reply('CMS page');
-    }
-});
-
-server.route({
-    method: 'GET',
-    path: '/edit/{id}',
-    handler: function (request, reply) {
-        reply('CMS page with post: ' + request.params.id + ' loaded for editing');
-    }
-});
-
-server.route({
-    method: 'POST',
-    config: { payload: {output: 'data', parse: true} },
-    path: '/',
-    handler: function (request, reply) {
-        // code here to handle new post
-        reply('New Post Added');
-    }
-});
-
-// PUT has a payload too..
-server.route({
-    method: 'PUT',
-    config: { payload: {output: 'data', parse: true} },
-    path: '/{id}',
-    handler: function (request, reply) {
-        // code here to handle post update
-        reply('Post ' + request.params.id + ' updated');
-    }
-});
-
-server.route({
-    method: 'DELETE',
-    path: '/{id}',
-    handler: function (request, reply) {
-        // code here to delete post
-        reply('Post ' + request.params.id + ' deleted');
-    }
-});
-
-
 //Github auth, use your own github application clientId and clientSecret keys
 server.register([Bell, AuthCookie], function (err) {
     if (err) {
         console.error(err);
         return process.exit(1);
     }
+
     var authCookieOptions = {
         password: 'cookie-encryption-password',
         cookie: 'hapiblog-auth',
         isSecure: false
     };
-
     server.auth.strategy('hapiblog-cookie', 'cookie', authCookieOptions);
+    
     var bellAuthOptions = {
         provider: 'github',
         password: 'github-encryption-password',
@@ -108,26 +45,28 @@ server.register([Bell, AuthCookie], function (err) {
         clientSecret: githubcreds.clientSecret,
         isSecure: false
     };
+    server.auth.strategy('github-oauth', 'bell', bellAuthOptions);
 
-server.auth.strategy('github-oauth', 'bell', bellAuthOptions);
+    server.auth.default('hapiblog-cookie');
 
-server.auth.default('hapiblog-cookie');
+//ROUTING
+    server.route([
 
-server.route([{
-    method: 'GET',
-    path: '/login',
-    config: {
-        auth: "github-oauth",
-        handler: function (request, reply) {
-            if (true) {
-                request.auth.session.clear();
-                request.auth.session.set(request.auth.credentials);
-                return reply('Hello ' + request.auth.credentials.profile.displayName);
+        {
+            method: 'GET',
+            path: '/login',
+            config: {
+                auth: 'github-oauth',
+                handler: function (request, reply) {
+                    if (true) {
+                        request.auth.session.clear();
+                        request.auth.session.set(request.auth.credentials);
+                        return reply('Hello ' + request.auth.credentials.profile.displayName);
+                    }
+                    reply('Not logged in...').code(401);    
+                }
             }
-            reply('Not logged in...').code(401);    
-        }
-    }
-},
+        },
 
         {
             method: 'GET',
@@ -143,9 +82,7 @@ server.route([{
             method: 'GET',
             path: '/',
             config: {
-                auth: {
-                    mode: 'optional'
-                },
+                auth: {mode: 'optional'},
                 handler: function (request, reply) {
                     if (request.auth.isAuthenticated) {
                         return reply('welcome back ' + request.auth.credentials.profile.displayName);
@@ -162,10 +99,76 @@ server.route([{
                 auth: false,
                 handler: function (request, reply) {
                     request.auth.session.clear();
-                    reply.redirect("/");
+                    reply.redirect('/');
+                }
+            }
+        },
+
+        {
+            method: 'GET',
+            path: '/jade',
+            config: {
+                auth: {mode: 'optional'},
+                handler: function(request, reply) {
+                    reply.view('homepage', {name: 'Batman'});
                 }
             }
         }
+
+//routes not using
+        // {
+        //     method: 'GET',
+        //     path: '/{id}',
+        //     handler: function (request, reply) {
+        //             reply('Blog Post here ' + request.params.id);
+        //         }
+        // },
+
+        // {
+        //     method: 'GET',
+        //     path: '/edit',
+        //     handler: function (request, reply) {
+        //         reply('CMS page');
+        //     }
+        // },
+
+        // {
+        //     method: 'GET',
+        //     path: '/edit/{id}',
+        //     handler: function (request, reply) {
+        //         reply('CMS page with post: ' + request.params.id + ' loaded for editing');
+        //     }
+        // },
+
+        // {
+        //     method: 'POST',
+        //     config: { payload: {output: 'data', parse: true} },
+        //     path: '/',
+        //     handler: function (request, reply) {
+        //         // code here to handle new post
+        //         reply('New Post Added');
+        //     }
+        // },
+
+        // {
+        //     method: 'PUT',
+        //     config: { payload: {output: 'data', parse: true} },
+        //     path: '/{id}',
+        //     handler: function (request, reply) {
+        //         // code here to handle post update
+        //         reply('Post ' + request.params.id + ' updated');
+        //     }
+        // },
+
+        // {
+        //     method: 'DELETE',
+        //     path: '/{id}',
+        //     handler: function (request, reply) {
+        //         // code here to delete post
+        //         reply('Post ' + request.params.id + ' deleted');
+        //     }
+        // }
+
     ]);
 });
 
